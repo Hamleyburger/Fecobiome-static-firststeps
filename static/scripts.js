2a3881e2f93678fada6e93fb871adfd295022bb9
Alma <alma9000@gmail.com>
2021-06-21 23:56:14 +0200

    smart and beautiful  navbar transition

---
+++ static/scripts.js
@@ -0,0 +1,8 @@
+// Make navbar toggle the "scrolled" class when being at scroll position past hero title
+ $(window).scroll(function(){
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+    // to find distance to title
+    var title_position = $(".hero-title").position();
+    console.log(title_position);
+ $('.nav-panel').toggleClass('scrolled', $(this).scrollTop() > title_position.top-60);
+ });
